307e2e714a9d96fe2225abf6d1496192c879842e
SamHuang <sam821203@yahoo.com.tw>
2022-09-14 00:29:03 +0800

    complete DOM project 3

--- JS-course-From Zero to Expert/DOM-and-event/project-3/script.js
+++ JS-course-From Zero to Expert/DOM-and-event/project-3/script.js
@@ -1,11 +1,108 @@
 'use strict';
 
 // Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
+const scoreEls = document.querySelectorAll('.score');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const currentEls = document.querySelectorAll('.current-score');
+
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 // Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+let scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+let playing = true;
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
+const resetGame = function () {
+  // active the button of roll and hold
+  playing = true;
+
+  currentScore = 0;
+  activePlayer = 0;
+  scores = [0, 0];
+
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.remove('player--active');
+  player1El.classList.remove('player--active');
+
+  player0El.classList.add('player--active');
+
+  // reset current and score content
+  for (let i = 0; i < currentEls.length; i++) {
+    currentEls[i].textContent = 0;
+    scoreEls[i].textContent = 0;
+  }
+};
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // 只有當 playing 是 true 時，按下 Roll 才能執行
+  if (playing) {
+    // 1. Generating a random dice btnRoll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+    // 2. Display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    // 3. Check for rolled 1: if true, switch to next player
+    if (dice !== 1) {
+      // Add dice to current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // Switch to next player
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    // 1. Add current score tp active player's score
+    scores[activePlayer] += currentScore;
+    console.log(scores[activePlayer]);
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    // 2. Check if player's score is >= 100
+    if (scores[activePlayer] >= 10) {
+      // Finish the game
+      playing = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      // Switch to the next player
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener('click', resetGame);
