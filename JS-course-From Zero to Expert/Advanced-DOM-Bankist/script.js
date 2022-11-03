4a2bffa89faaf575e3908badb46f86a0329897b8
SamHuang <sam821203@yahoo.com.tw>
2022-11-03 22:57:17 +0800

    complete 188

--- JS-course-From Zero to Expert/Advanced-DOM-Bankist/script.js
+++ JS-course-From Zero to Expert/Advanced-DOM-Bankist/script.js
@@ -74,3 +74,89 @@ document
     // 以前的做法
     message.parentElement.removeChild(message);
   });
+
+// Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+// 但這種做法只能拿到 inline style 裡的屬性值
+console.log(message.style.height);
+console.log(message.style.backgroundColor);
+
+// 也可以用這種方式來娶某個元素上的屬性值
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 40 + 'px';
+// message.style.height = getComputedStyle(message).height;
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(typeof logo.className);
+// 會是 undefined，因為不是 img 上一般標準的屬性
+// 但可以用 getAttribute 來得到客製化的值
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+
+logo.setAttribute('company', 'BankList');
+
+// 拿到圖片絕對路徑和相對路徑的方法
+console.log(logo.src);
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// Data Attributes
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('c');
+logo.classList.remove('c');
+logo.classList.toggle('c');
+
+// 跟陣列在用的 includes 不一樣
+logo.classList.contains('c');
+
+// 不用要這個，因為它會覆蓋掉該元素上所有的 class
+// logo.className = 'Jonas';
+
+// Smooth Scroll
+// 較舊的方法
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  // 這裡的 target 是指 btnScrollTo
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  // 較新的方法
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
