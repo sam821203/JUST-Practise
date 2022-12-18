3be8843f3b05429b68806e44d67a6e0ddfd3359a
SamHuang <sam821203@yahoo.com.tw>
2022-12-18 23:07:17 +0800

    了解為何用兩次 .then()

--- AJAX-fundamentals-simply-put/fetch/response.js
+++ AJAX-fundamentals-simply-put/fetch/response.js
@@ -1,12 +1,15 @@
-
 // set Fetch request
 // let url = "https://od.moi.gov.tw/api/v1/rest/datastore/301110000A-000395-005";
 // https://www.etmall.com.tw/kanban/Get?Type=0
 // Your eBird API key: n1rh2qh3b5g
 
 let url = "https://jsonplaceholder.typicode.com/todos/1";
-console.log('hi');
 fetch(url)
   .then((response) => {
-    console.log(response.headers.get('content-type'));
+    // let jsonData = JSON.parse(response);
+    // console.log(jsonData);
+    return response.json();
   })
+  .then((data) => {
+    console.log(data);
+  });
