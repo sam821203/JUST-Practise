c49ab6484d936cf08c2143a2e373b563244511c3
sam <sam821203@yahoo.com.tw>
2023-01-09 18:23:25 +0800

    update main visual to follow touch firecracker

--- following-touch-firecracker-test/script.js
+++ following-touch-firecracker-test/script.js
@@ -7,7 +7,7 @@ const animationDuration = 0.6;
 
 function initTouchAnimate() {
   document.addEventListener("touchmove", animateFirecrackerMove);
-  // document.addEventListener("touchend", animateFirecrackerEnd);
+  document.addEventListener("touchend", animateFirecrackerEnd);
 }
 
 function removeAllChildNodes(parent) {
@@ -21,24 +21,21 @@ function animateFirecrackerMove(touches) {
   const touchWrap = document.querySelector(".touch__wrap");
   
   // 設定隨機顏色
-  // const colors = ["rgba(255, 255, 255, 0.8)", "rgba(255, 255, 255, 0.6)", "rgba(255, 255, 255, 0.4)", "rgba(255, 255, 255, 0.2)"];
+  const colors = ["#0066E7", "#D4070F", "#FFDA13"];
 
   // 偵測不同手指
   [...touches.changedTouches].forEach((touch) => {
 
     const firecracker = document.createElement("div");
-    const coin = document.createElement("img");
 
     // 給予隨機的顏色
-    // let color = colors[Math.floor(Math.random() * colors.length)];
-
-    coin.setAttribute("src", 'money.png');
+    let color = colors[Math.floor(Math.random() * colors.length)];
 
     // 偵測 X 與 Y 軸
     firecracker.classList.add('firecracker');
     firecracker.style.top = `${touch.clientY}px`;
     firecracker.style.left = `${touch.clientX}px`;
-    // firecracker.style.backgroundColor = color;
+    firecracker.style.backgroundColor = color;
 
     // 讓生成出來的元素能有過渡的時間
     // 把 settimeout 放來這裡
@@ -51,7 +48,6 @@ function animateFirecrackerMove(touches) {
     // firecracker.style.opacity = 0;
 
     // 新增至頁面上
-    firecracker.appendChild(coin)
     touchWrap.append(firecracker);
 
     // setTimeout( function() {
