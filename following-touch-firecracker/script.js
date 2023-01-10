7d935432891dfbe5bf0e988403e09b30a8dd2410
sam <sam821203@yahoo.com.tw>
2023-01-10 17:49:49 +0800

    complete both touch and mousemove event

--- following-touch-firecracker/script.js
+++ following-touch-firecracker/script.js
@@ -1,72 +1,65 @@
+const animationDuration = 1.4;
+      const touchWrap = document.querySelector(".touch__wrap");
+      
+      document.addEventListener("DOMContentLoaded", buildTouchAnimate);
 
+      function randomInRange (min, max) {
+        return Math.floor(Math.random() * (max - min) + 1) + min;
+      }
 
+      function buildTouchAnimate() {
+        document.addEventListener("touchmove", animateFirecrackerMove);
+        document.addEventListener("touchend", animateFirecrackerEnd);
+      }
 
-document.addEventListener("DOMContentLoaded", initTouchAnimate);
+      function removeAllChildNodes(parent) {
+        while (parent.firstChild) {
+          parent.removeChild(parent.firstChild);
+        };
+      };
 
-const animationDuration = 0.6;
+      // touch move
+      function animateFirecrackerMove(touches) {
+        
+        // 偵測不同手指
+        [...touches.changedTouches].forEach((touch) => {
 
-function initTouchAnimate() {
-  document.addEventListener("touchmove", animateFirecrackerMove);
-  // document.addEventListener("touchend", animateFirecrackerEnd);
-}
+          const firecracker = document.createElement("div");
 
-function removeAllChildNodes(parent) {
-  while (parent.firstChild) {
-    parent.removeChild(parent.firstChild);
-  };
-};
+          firecracker.classList.add('sparkler');
+          
+          for (let count = 0; count < 10; count++) {
+            firecracker.insertAdjacentHTML('beforeend', `
+            <div class="sparkler__spark" style="
+              --rotation: ${randomInRange(0, 360)}; 
+              --delay: ${Math.random()}; 
+              --speed: ${randomInRange(1, 10) / 10}; 
+              --travel: ${randomInRange(100, 150)}; 
+              --h: ${randomInRange(20, 60)}; 
+              --s: ${randomInRange(50, 100)}; 
+              --l: ${randomInRange(50, 100)};">
+            </div>
+          `);
+          }
+          
+          // 偵測 X 與 Y 軸
+          firecracker.style.top = `${touch.clientY}px`;
+          firecracker.style.left = `${touch.clientX}px`;
 
-// touch move
-function animateFirecrackerMove(touches) {
-  const touchWrap = document.querySelector(".touch__wrap");
-  
-  // 設定隨機顏色
-  const colors = ["rgba(255, 255, 255, 0.6)", "rgba(255, 255, 255, 0.4)", "rgba(255, 255, 255, 0.2)"];
+          // 加上淡入淡出動畫
+          firecracker.style.animation = `fade-out ${animationDuration}s ease-out forwards`;
 
-  // 偵測不同手指
-  [...touches.changedTouches].forEach((touch) => {
+          // 新增至頁面上
+          touchWrap.appendChild(firecracker);
+        });
+      }
 
-    const firecracker = document.createElement("div");
-    // const coin = document.createElement("img");
-
-    // 給予隨機的顏色
-    let color = colors[Math.floor(Math.random() * colors.length)];
-
-    // coin.setAttribute("src", 'money.png');
-
-    // 偵測 X 與 Y 軸
-    firecracker.classList.add('firecracker');
-    firecracker.style.top = `${touch.clientY}px`;
-    firecracker.style.left = `${touch.clientX}px`;
-    firecracker.style.backgroundColor = color;
-
-    // 讓生成出來的元素能有過渡的時間
-    // 把 settimeout 放來這裡
-    firecracker.style.animation = `fade-out ${animationDuration}s ease-out forwards`;
-
-    // firecracker.style.width = "100px";
-    // firecracker.style.height = "100px";
-
-    // firecracker.style.borderWidth = "5px";
-    // firecracker.style.opacity = 0;
-
-    // 新增至頁面上
-    // firecracker.appendChild(coin)
-    touchWrap.append(firecracker);
-
-    // setTimeout( function() {
-    //   removeAllChildNodes(touchWrap);
-    // }, 2000);
-  });
-}
-
-// touch end
-function animateFirecrackerEnd() {
-  const touchWrap = document.querySelector(".touch__wrap");
-  const delayTime =  animationDuration * 2 * 1000;
-  
-  setTimeout( function() {
-    removeAllChildNodes(touchWrap);
-  }, delayTime);
-};
-
+      // touch end
+      function animateFirecrackerEnd() {
+        const delayTime =  animationDuration * 2 * 1000;
+        
+        // 如果 opacity = 0，則刪除該節點
+        setTimeout( function() {
+          removeAllChildNodes(touchWrap);
+        }, delayTime);
+      };
