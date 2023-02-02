08236e1b79a505ed8c9509d8d3b5e2b486367e6d
sam <sam821203@yahoo.com.tw>
2023-02-02 18:21:38 +0800

    basics 5

---
+++ Vue-the-complete-guide/basics-05-using-the-native-event-object/app.js
@@ -0,0 +1,31 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: ''
+    };
+  },
+  methods: {
+    outputFullName() {
+      if (this.name === '') {
+        return '';
+      }
+      return this.name + ' ' + 'Huang'
+    },
+    setName(event, lastName) {
+      this.name = event.target.value;
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+      // this.counter--;
+    },
+    resetInput() {
+      this.name = '';
+    }
+  }
+});
+
+app.mount('#events');
