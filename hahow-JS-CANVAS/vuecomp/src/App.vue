83e54644876872f689670b475404b840392e1e4e
SamHuang <sam821203@yahoo.com.tw>
2023-02-19 12:32:39 +0800

    using

---
+++ hahow-JS-CANVAS/vuecomp/src/App.vue
@@ -0,0 +1,67 @@
+<template lang="pug">
+  #app
+    h1 記帳程式
+    hr  
+    RecordList(v-bind:records="records"
+              @removeListItem="list1RemoveItem")
+    h2 第二個人
+    RecordList(v-bind:records="records2")
+
+
+</template>
+
+<script>
+import RecordList from "./components/RecordList.vue";
+export default {
+  name: "app",
+  data() {
+    return {
+      msg: "Welcome to first vue test app!",
+      records: [
+        {
+          date: "2018/8/1",
+          price: 500,
+          cata: "食物"
+        },
+        {
+          date: "2018/8/3",
+          price: 800,
+          cata: "衣服"
+        },
+        {
+          date: "2018/8/8",
+          price: 1200,
+          cata: "手機"
+        }
+      ],
+      records2: [
+        {
+          date: "2018/8/3",
+          price: 540,
+          cata: "食物"
+        },
+        {
+          date: "2018/8/9",
+          price: 820,
+          cata: "衣服"
+        },
+        {
+          date: "2018/8/10",
+          price: 1500,
+          cata: "手機"
+        }
+      ]
+    };
+  },
+  methods: {
+    list1RemoveItem() {
+      console.log("App receive event from child");
+    }
+  },
+  components: {
+    RecordList: RecordList
+  }
+};
+</script>
+
+<style lang="sass"></style>
---
+++ hahow-JS-CANVAS/vuecomp/src/components/RecordList.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+  ul.record
+    li(v-for="record in records")
+      span.cata {{ record.cata }}
+      span.date {{ record.date }}
+      span.price {{ record.price }} 元
+
+      button(@click="toggleRemove(record)") 移除
+</template>
+
+<script>
+export default {
+  props: ["records"],
+  data() {
+    return {};
+  },
+  methods: {
+    toggleRemove(record) {
+      console.log("RecordList Toggle Event");
+      this.$emit("removeListItem");
+    }
+  }
+};
+</script>
+
+<style lang="sass">
+.record
+  li
+    display: flex
+    padding: 5px
+    border: 2px solid #000
+    .cata
+      flex: 4
+    .date
+      flex: 2
+    .date
+      flex: 2
+</style>
