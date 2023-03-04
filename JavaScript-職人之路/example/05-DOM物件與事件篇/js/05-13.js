e2049bfc9671f59f140eea0b820e73bb188de6a8
SamHuang <sam821203@yahoo.com.tw>
2023-03-04 14:31:26 +0800

    complete JS chapter 5

--- JavaScript-職人之路/example/05-DOM物件與事件篇/js/05-13.js
+++ JavaScript-職人之路/example/05-DOM物件與事件篇/js/05-13.js
@@ -1,30 +1,19 @@
-window.onload = function(){
+var contentPhone = document.getElementById("contentPhone");
+var a1 = document.getElementById("a1");
+var a2 = document.getElementById("a2");
+var a3 = document.getElementById("a3");
+var a4 = document.getElementById("a4");
+var a5 = document.getElementById("a5");
 
-    var a1 = document.getElementById("a1");
-    var a2 = document.getElementById("a2");
-    var a3 = document.getElementById("a3");
-    var a4 = document.getElementById("a4");
-    var a5 = document.getElementById("a5");
-    var phone = document.getElementById("contentPhone");
+window.onload = function () {};
 
-    a1.addEventListener("click", function(){
-        phone.style.backgroundImage = 'url("../images/05/big/a1.jpg")';
-    });
-
-    a2.addEventListener("click", function(){
-        phone.style.backgroundImage = 'url("../images/05/big/a2.jpg")';
-    });
-
-    a3.addEventListener("click", function(){
-        phone.style.backgroundImage = 'url("../images/05/big/a3.jpg")';
-    });
-
-    a4.addEventListener("click", function(){
-        phone.style.backgroundImage = 'url("../images/05/big/a4.jpg")';
-    });
-
-    a5.addEventListener("click", function(){
-        phone.style.backgroundImage = 'url("../images/05/big/a5.jpg")';
-    });
-
+function showPhoto() {
+  contentPhone.style.backgroundImage =
+    'url("../images/05/big/' + this.id + '.jpg")';
 }
+
+a1.addEventListener("click", showPhoto);
+a2.addEventListener("click", showPhoto);
+a3.addEventListener("click", showPhoto);
+a4.addEventListener("click", showPhoto);
+a5.addEventListener("click", showPhoto);
--- JavaScript-職人之路/example/05-DOM物件與事件篇/js/05-16.js
+++ JavaScript-職人之路/example/05-DOM物件與事件篇/js/05-16.js
@@ -1,44 +1,45 @@
-window.onload = function(){
-    
-    var a1 = document.getElementById("a1");
-    var a2 = document.getElementById("a2");
-    var a3 = document.getElementById("a3");
-    var a4 = document.getElementById("a4");
-    var a5 = document.getElementById("a5");
-    var phone = document.getElementById("contentPhone");
-    
-    var angleleft = document.getElementById("angleleft");
-    var angleright = document.getElementById("angleright");
-    var pageIdx = 1;
+window.onload = function () {
+  var a1 = document.getElementById("a1");
+  var a2 = document.getElementById("a2");
+  var a3 = document.getElementById("a3");
+  var a4 = document.getElementById("a4");
+  var a5 = document.getElementById("a5");
+  var phone = document.getElementById("contentPhone");
 
-    a1.addEventListener("click", showPhoto);
-    a2.addEventListener("click", showPhoto);
-    a3.addEventListener("click", showPhoto);
-    a4.addEventListener("click", showPhoto);
-    a5.addEventListener("click", showPhoto);
-    angleleft.addEventListener("click", angleleftFn)
-    angleright.addEventListener("click", anglerightFn)
+  const angleLeft = document.getElementById("angleLeft");
+  const angleRight = document.getElementById("angleRight");
 
-    function showPhoto(){
-        pageIdx = Number(this.id.substr(1));
-        phone.style.backgroundImage = 'url("../images/05/big/' + this.id +'.jpg")';
-    }
+  let idIndex = 1;
 
-    function angleleftFn(){
-        pageIdx = pageIdx - 1;
-        if(pageIdx < 1){
-            pageIdx = 5;
-        }
-        phone.style.backgroundImage = 'url("../images/05/big/a' + pageIdx +'.jpg")';
-    }
-    function anglerightFn(){
-        pageIdx = pageIdx + 1;
-        if(pageIdx > 5){
-            pageIdx = 1;
-        }
-        phone.style.backgroundImage = 'url("../images/05/big/a' + pageIdx +'.jpg")';
-    }
+  a1.addEventListener("click", showPhoto);
+  a2.addEventListener("click", showPhoto);
+  a3.addEventListener("click", showPhoto);
+  a4.addEventListener("click", showPhoto);
+  a5.addEventListener("click", showPhoto);
 
+  angleLeft.addEventListener("click", leftFunction);
+  angleRight.addEventListener("click", rightFunction);
 
+  function showPhoto() {
+    idIndex = Number(this.id.substr(1));
+    phone.style.backgroundImage = 'url("../images/05/big/' + this.id + '.jpg")';
+  }
 
-}
+  function leftFunction() {
+    idIndex--;
+
+    if (idIndex === 0) idIndex = 5;
+
+    phone.style.backgroundImage =
+      'url("../images/05/big/a' + idIndex + '.jpg")';
+  }
+
+  function rightFunction() {
+    idIndex++;
+
+    if (idIndex > 5) idIndex = 1;
+
+    phone.style.backgroundImage =
+      'url("../images/05/big/a' + idIndex + '.jpg")';
+  }
+};
