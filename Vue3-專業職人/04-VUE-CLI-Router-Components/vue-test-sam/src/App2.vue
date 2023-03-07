3f045ed7eb4bd41b1caf6331a33d60a67f73fdb4
sam <sam821203@yahoo.com.tw>
2023-03-07 18:19:00 +0800

    complete Vue chapter 7-5

---
+++ Vue3-專業職人/04-VUE-CLI-Router-Components/vue-test-sam/src/App2.vue
@@ -0,0 +1,44 @@
+<script>
+import Header from "@/components/Header";
+import Article from "@/components/Article";
+import Aside from "@/components/Aside";
+import Main from "@/components/Main";
+import Footer from "@/components/Footer";
+
+export default {
+  components: {
+    Header,
+    Article,
+    Aside,
+    Main,
+    Footer,
+  }
+}
+</script>
+
+<template>
+  <Header />
+  <Article />
+  <Aside />
+  <Main />
+  <Footer />
+</template>
+
+<style>
+  * {
+    margin: 0;
+    padding: 0;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+    font-family: "Microsoft JhengHei", "Heiti TC", "sans-serif";
+  }
+  img {
+    display: block;
+  }
+  html,
+  body {
+    width: 100%;
+    height: 100%;
+  }
+</style>
