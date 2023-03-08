e132752be50e3c54e32c2470586e69859336b219
sam <sam821203@yahoo.com.tw>
2023-03-08 18:01:55 +0800

    complete Vue chapter 7-10

---
+++ Vue3-專業職人/04-VUE-CLI-Router-Components/props/src/components/PropsTest.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  props: ["msg"],
+  setup(props) {
+    console.log(props);
+    return {
+      props,
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ props.msg }}</h1>
+</template>
+
+<style>
+  h1 {
+    color: aquamarine;
+    font-size: 50px;
+  }
+</style>
