996fbad47ee630c80685ca3d8914b8b4863265ab
SamHuang <sam821203@yahoo.com.tw>
2023-03-14 08:34:19 +0800

    complete Vue chapter 7

--- Vue3-專業職人/04-VUE-CLI-Router-Components/props/src/App.vue
+++ Vue3-專業職人/04-VUE-CLI-Router-Components/props/src/App.vue
@@ -1,10 +1,11 @@
 <script>
-import TitleBar from '@/components/TitleBar.vue'
-import ListItem from '@/components/ListItem.vue'
-import TimerBox from '@/components/TimerBox.vue'
-import EventBack from '@/components/EventBack.vue'
-import TemplateRefs from '@/components/TemplateRefs.vue'
-import { ref } from 'vue'
+import TitleBar from "@/components/TitleBar.vue";
+import ListItem from "@/components/ListItem.vue";
+import TimerBox from "@/components/TimerBox.vue";
+import EventBack from "@/components/EventBack.vue";
+import TemplateRefs from "@/components/TemplateRefs.vue";
+import SlotsTitle from "@/components/SlotsTitle.vue";
+import { ref } from "vue";
 
 export default {
   components: {
@@ -13,6 +14,7 @@ export default {
     TimerBox,
     EventBack,
     TemplateRefs,
+    SlotsTitle,
   },
   setup() {
     const isAnime = ref(false);
@@ -23,12 +25,12 @@ export default {
     };
 
     const handleTimeOut = (num) => {
-      console.log(`Time is over: ${num.value}` );
-    }
+      console.log(`Time is over: ${num.value}`);
+    };
 
     const handleAnimeClick = () => {
       isAnime.value = !isAnime.value;
-    }
+    };
 
     return {
       isOpen,
@@ -36,9 +38,9 @@ export default {
       handleTimeOut,
       // isAnime,
       // handleAnimeClick,
-    }
-  }
-}
+    };
+  },
+};
 </script>
 
 <template>
@@ -48,7 +50,23 @@ export default {
 
   <!-- <EventBack /> -->
 
-  <TemplateRefs />
+  <!-- <TemplateRefs /> -->
+
+  <slots-title>
+    <div>
+      <a href="">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat, esse
+        adipisci, qui sed iste quasi recusandae nisi nostrum optio harum
+        deserunt quas. Minus neque perferendis, a officia voluptate quos minima!
+      </a>
+    </div>
+  </slots-title>
+  <slots-title>
+    <p>test2</p>
+  </slots-title>
+  <slots-title>
+    <p>test3</p>
+  </slots-title>
 
   <!-- <button @click="handleAnimeClick">click</button>
   <transition name="fade">
@@ -57,7 +75,7 @@ export default {
 </template>
 
 <style>
-  /* .fade-enter-active {
+/* .fade-enter-active {
     animation: bounce-in 0.5s;
   }
 
@@ -65,7 +83,7 @@ export default {
     animation: bounce-in 0.5s reverse;
   } */
 
-  /* .fade-enter-from,
+/* .fade-enter-from,
   .fade-leave-to {
     opacity: 0;
   }
@@ -75,7 +93,7 @@ export default {
     opacity: 1;
   } */
 
-  /* @keyframes bounce-in {
+/* @keyframes bounce-in {
     0% {
       transform: scale(0);
     }
@@ -87,30 +105,30 @@ export default {
     }
   } */
 
-  * {
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
-  }
-  html,
-  body {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    height: 100%;
-    background-color: slategray;
-  }
-  #app {
-    width: 400px;
-    overflow: hidden;
-    border: 1px solid #42b983;
-    border-radius: 10px;
-  }
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+html,
+body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background-color: slategray;
+}
+#app {
+  width: 400px;
+  overflow: hidden;
+  border: 1px solid #42b983;
+  border-radius: 10px;
+}
 
-  #box {
-    width: 100px;
-    height: 100px;
-    background-color: red;
-  }
+#box {
+  width: 100px;
+  height: 100px;
+  background-color: red;
+}
 </style>
---
+++ Vue3-專業職人/04-VUE-CLI-Router-Components/props/src/components/SlotsTitle.vue
@@ -0,0 +1,27 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <h1>
+    <img src="../assets/logo.png" alt="" />
+    <slot></slot>
+  </h1>
+</template>
+
+<style scoped>
+h1 {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 30px;
+  color: #fff;
+  font-size: 30px;
+}
+
+img {
+  display: block;
+  width: 25px;
+  margin-right: 10px;
+}
+</style>
