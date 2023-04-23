42a7dbe70673dd79b1f2550c6c9c0f1c39cf97f7
SamHuang <sam821203@yahoo.com.tw>
2023-04-23 15:44:20 +0800

    complete JS course intermediate chapter 5

--- Mike-JavaScript-course-intermediate/exercise/05-共用組件載入篇/js/main.js
+++ Mike-JavaScript-course-intermediate/exercise/05-共用組件載入篇/js/main.js
@@ -1,4 +1,25 @@
-window.onload = function(){
+window.onload = function () {
 
-    
+  const headerEl = document.getElementById('header');
+  const footerEL = document.getElementById('footer');
+
+  function headerData() {
+    return axios.get('./components/header.html');
+  }
+
+  const footerData = () => axios.get('./components/footer.html');
+
+  axios.all([headerData(), footerData()]).then(axios.spread(function (header, footer) {
+    headerEl.innerHTML = header.data;
+    footerEL.innerHTML = footer.data;
+    // console.log(header, footer);
+  }))
+
+  // axios.get('./components/header.html').then(function (res) {
+  //   headerEl.innerHTML = res.data;
+  // })
+
+  // axios.get('./components/footer.html').then(function (res) {
+  //   footerEl.innerHTML = res.data;
+  // })
 }
